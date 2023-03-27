833bda553ef7d4341f768fe311bb20fd4dbb090d
Phyper14 <martins14.pablo@gmail.com>
2023-03-27 17:50:47 -0300

    Add logo

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,7 +3,10 @@ import styles from "./Cardapio.module.scss";
 export default function Cardapio() {
 	return (
 		<main>
-			<nav className={styles.menu}>Aluroni</nav>
+			<nav className={styles.menu}>
+				<img src="../../assets/logo.svg" alt="logo Aluroni" />
+				Aluroni
+			</nav>
 		</main>
 	);
 }
